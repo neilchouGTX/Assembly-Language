2506f675f05a88394da50419491c527facc274c3
neilchouGTX <neil12386@hotmail.com.tw>
2020-11-03 19:49:57 +0800

    modifed HW03

--- PrimeHW03/Prime/Prime/Source.asm
+++ PrimeHW03/Prime/Prime/Source.asm
@@ -15,73 +15,75 @@ powString BYTE "^" , 0
 
 .code
 main PROC
-master:
-	call readInt
-	mov num, EAX
-	cmp num, 0			;if num < 0 then jump to finish
-	jl finish
-	mov flag, 1			;reset flag to 1
-	call PrimeFac		;call PrimeFac to process the number
-	call crlf
-	mov ECX, 10			;make loop infinity
-	loop master
-finish:
-INVOKE ExitProcess, 0
+	master:
+		call readInt
+		mov num, EAX
+		cmp num, 0			;if num < 0 then jump to finish
+		jl finish
+		je one
+		mov flag, 1			;reset flag to 1
+		call PrimeFac		;call PrimeFac to process the number
+		one:
+		call crlf
+		mov ECX, 10			;make loop infinity
+		loop master
+	finish:
+	INVOKE ExitProcess, 0
 main endp
 
 PrimeFac PROC
-mov temp, 1				;set the divisor
-anotherTemp:
-	inc temp
-	mov count, 0		;reset the power count
-	TestLB:
-		mov EAX, num
-		cdq
-		div temp
-		cmp EDX, 0			;if the reminder is 0 than count+1 else jump to nofac
-		jne nofac
-		mov num, EAX
-		inc count
-		mov ECX , 5			;make loop infinity
-		loop TestLB
-	nofac:
-		cmp count, 0		;if count still 0 means that this divisor is not correct jump to next
-		je next
-		call print
-		mov flag, 0
-		next:
-		mov EAX, num
-		cmp temp, EAX		;if temp > num then jump to fin
-		jae fin
-		mov ECX, 5			;make loop infinity
-	loop anotherTemp
-	fin:
-	ret
+	mov temp, 1				;set the divisor
+	anotherTemp:
+		inc temp
+		mov count, 0		;reset the power count
+		TestLB:
+			mov EAX, num
+			cdq
+			div temp
+			cmp EDX, 0			;if the reminder is 0 than count+1 else jump to nofac
+			jne nofac
+			mov num, EAX
+			inc count
+			mov ECX , 5			;make loop infinity
+			loop TestLB
+		nofac:
+			cmp count, 0		;if count still 0 means that this divisor is not correct jump to next
+			je next
+			call print
+			mov flag, 0
+			next:
+			mov EAX, num
+			cmp temp, EAX		;if temp > num then jump to fin
+			jae fin
+			mov ECX, 5			;make loop infinity
+		loop anotherTemp
+		fin:
+		ret
 PrimeFac endp
 
 printmul PROC
-mov edx, OFFSET mulString		;print multiple symbol
-call writeString
-ret
+	mov edx, OFFSET mulString		;print multiple symbol
+	call writeString
+	ret
 printmul endp
 
 print PROC
 
-cmp flag ,0					;check whether printmul need to print 
-jne nomul
-	call printmul
+	cmp flag ,0					;check whether printmul need to print 
+	jne nomul
+		call printmul
 	
-	nomul:
-		mov EAX,temp
-		call writeDec
-		cmp count, 1			;if the power count is 1 then we do not need to print the power
-		je nopow
-		mov edx, OFFSET powString
-		call writeString
-		mov EAX, count
-		call writeDec
-	nopow:
-ret
+		nomul:
+			mov EAX,temp
+			call writeDec
+			cmp count, 1			;if the power count is 1 then we do not need to print the power
+			je nopow
+			mov edx, OFFSET powString
+			call writeString
+			mov EAX, count
+			call writeDec
+		nopow:
+	ret
 print endp
 
 END main
