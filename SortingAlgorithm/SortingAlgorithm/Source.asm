f78ecb47c534857e8ade18de45eafffdfadfa444
neilchouGTX <neil12386@hotmail.com.tw>
2020-11-18 00:15:11 +0800

    new file

--- SortingAlgorithm/SortingAlgorithm/Source.asm
+++ SortingAlgorithm/SortingAlgorithm/Source.asm
@@ -1,37 +1,99 @@
 INCLUDE prototype.inc
 
-
 .data
 arr SDWORD 100000 DUP(? )
 inputline BYTE "Enter an integer:", 0
+space BYTE " ",0
 count DWORD ?
-
-
 
 .code
 main PROC
-	call readInt
-	mov count,eax
-	mov ecx, count
-	mov esi, OFFSET arr
-	input:
-		call readInt
-		mov [esi],eax
-		add esi,4
-		loop input
-	INVOKE sort, ADDR arr, count
-	mov esi, OFFSET arr
-	mov ecx,count
-	L1:
-		mov eax,[esi]
-		call writeInt
-		add esi,4
-		loop L1
-
-
-
-
-	exit
+	round:
+		call readInt				;read N
+		cmp eax,0
+		je quit						;if N = 0 then quit
+		mov count,eax
+		mov ecx, count				;make input loop "count" times
+		mov esi, OFFSET arr
+		input:
+			call readInt
+			mov [esi],eax
+			add esi,4
+			loop input
+		INVOKE sort, ADDR arr, count		;invoke sort and pass  2 parameters
+		call printSort				
+		call crlf
+		call printMid
+		call crlf
+		jmp round					;infinite loop
+	quit:
 	INVOKE ExitProcess, 0 
-main endp
+main ENDP
+
+printSort PROC
+	mov esi, OFFSET arr
+	mov ecx, count				;loop count times to print the element in array
+	L1 :
+		mov eax, [esi]
+		cmp eax,0			;if [esi]>0 then dont need to print '+'
+		Jl INTT
+		DECC:
+			call writeDec
+			jmp Con
+		INTT:
+			call writeInt
+		Con:
+		mov edx, offset space
+		call writeString
+		add esi, 4				;take next element
+		loop L1
+	ret
+printSort ENDP
+
+printMid PROC
+	mov eax,count
+	mov edx,0
+	mov ebx,2
+	div ebx
+	cmp edx,0			;compare the remainder is 0 or 1
+	ja ODD
+	EVENN:
+		mov ecx,2				;print two times
+		mov esi, OFFSET arr
+		mov ebx,4				
+		mul ebx					;4 bype
+		add esi, eax
+		sub esi, 4				;the middle left one
+		mid:
+			mov eax,[esi]
+			cmp eax,0
+			Jl INTT
+			DECC:
+				call writeDec
+				jmp Con
+			INTT:
+				call writeInt
+			Con:
+			mov edx, offset space
+			call writeString
+			add esi,4			;take next element(middle right one)
+			loop mid
+			jmp oddCon
+	ODD:
+		mov esi, OFFSET arr
+		mov ebx,4
+		mul ebx
+		add esi, eax
+		mov eax,[esi]
+		cmp eax,0
+		Jl oddINTT
+		oddDECC:
+			call writeDec
+			jmp oddCon
+		oddINTT:
+			call writeInt
+	oddCon:
+
+	ret
+printMid ENDP
 end main
--- SortingAlgorithm/SortingAlgorithm/_sort.asm
+++ SortingAlgorithm/SortingAlgorithm/_sort.asm
@@ -1,8 +1,9 @@
 INCLUDE prototype.inc
 
+
 .data
 i DWORD 1
-j DWORD ?
+j SDWORD 1
 key SDWORD ?
 
 .code 
@@ -10,43 +11,38 @@ sort PROC,
 	ptrArray: PTR SDWORD,
 	arraySize : DWORD
 
-
-	mov esi, ptrArray
-	mov ecx, arraySize
-	L2:
-		mov eax,[esi]
-		call writeInt
-		add esi,4
-		loop L2
-
+	mov i,1
+	mov j,1
 	mov ecx,arraySize
 	dec ecx
-	mov esi, 4
+	mov esi, ptrArray
+	add esi,4
 	L1:
-		mov eax, ptrArray[esi]
+		mov eax, [esi]
 		mov key, eax
-		mov ebx, esi
-		sub ebx,4
-		push ecx
+		push esi
+		sub esi,4
+		mov eax,i
+		mov j, eax
+		dec j
 		Testt:
-			cmp ebx,0
-			jb  exitt
+			cmp j,0
+			jl  exitt
 			mov eax,key
-			;call writeInt
-			cmp ptrArray[ebx],eax
-			jle  exitt
-			mov eax, ptrArray[ebx]
-			mov ptrArray[ebx+4],eax
-			sub ebx,4
-			mov ecx,5
-			loop Testt
+			cmp [esi],eax
+			jge  exitt
+			mov eax, [esi]
+			mov [esi+4],eax
+			sub esi,4
+			dec j
+			jmp Testt
 		exitt:
-		pop ecx
 		mov eax,key
-		mov ptrArray[ebx+4],eax
+		mov [esi+4],eax
+		pop esi
 		add esi,4
+		inc i
 		loop L1
-
 
 	ret
 sort endp
